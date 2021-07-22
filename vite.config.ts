df8005ad6eabcfaf4a66a54951457f7b5d16da5b
Ben Cheng <bencheng@skylight-group.com>
2021-07-22 15:42:50 +0800

    add: vite eslint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import viteESLint from '@ehutch79/vite-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteESLint()],
   server: {
     fs: {
       strict: false,
