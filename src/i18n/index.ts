6844e728c92f9afd7344bc98343c10b45ba031bb
Ben Cheng <bencheng@skylight-group.com>
2021-07-23 15:41:05 +0800

    fix: disable warn (not work)

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,10 +3,13 @@ import en from './en.json';
 
 const messages = {
   en,
+  'en-US': en,
 };
 
 export const i18n = createI18n({
   legacy: false,
   locate: navigator.language,
   messages: messages,
+  silentFallbackWarn: true,
+  silentTranslationWarn: true,
 });
