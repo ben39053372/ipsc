7e56618509dba9592bd154ecab895592112973fc
ben39053372 <ben39053372@gmail.com>
2021-07-26 21:07:51 +0800

    feat: button style

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,11 +1,24 @@
 <template>
-  <button type="button"><slot /></button>
+  <button :class="[color, outline ? 'outline' : '']" type="button">
+    <slot />
+  </button>
 </template>
 
 <script>
   import { defineComponent } from 'vue';
   export default defineComponent({
     name: 'Button',
+    props: {
+      outline: {
+        type: Boolean,
+        default: false,
+      },
+      color: {
+        type: String,
+        enum: ['primary', 'secondary', 'error', 'warn'],
+        default: 'primary',
+      },
+    },
   });
 </script>
 
@@ -15,7 +28,50 @@
     padding: 8px 20px;
     color: var(--text-color);
     border-radius: 4px;
-    border: 0;
+    border-width: 2;
+    border-style: solid;
     font-weight: 700;
+    transition: transform 0.1s;
+  }
+
+  button:active {
+    filter: brightness(55%);
+  }
+
+  button:hover {
+    transform: scale(1.05);
+  }
+
+  .primary {
+    color: var(--main-color);
+    border-color: var(--main-color);
+    background: var(--main-color);
+  }
+
+  .secondary {
+    color: var(--secound-color);
+    border-color: var(--secound-color);
+    background: var(--secound-color);
+  }
+
+  .error {
+    color: var(--error-color);
+    border-color: var(--error-color);
+    background: var(--error-color);
+  }
+
+  .warn {
+    color: var(--warn-color);
+    border-color: var(--warn-color);
+    background: var(--warn-color);
+  }
+
+  button:not(.outline) {
+    color: var(--text-color);
+  }
+
+  .outline {
+    background: #0001;
+    border-width: 2px;
   }
 </style>
