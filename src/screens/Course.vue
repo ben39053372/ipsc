0bc7284a9ee4a3ece4975107f098e27cb71a74ae
Ben Cheng <bencheng@skylight-group.com>
2021-08-02 15:26:42 +0800

    revamp: use container

--- src/screens/Course.vue
+++ src/screens/Course.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="container">
+  <Container class="container">
     <CourseHeader />
     <CourseScoreboard />
-  </div>
+  </Container>
 </template>
 <script lang="ts">
   import { defineComponent } from 'vue';
   import CourseHeader from '../components/CourseHeader.vue';
   import CourseScoreboard from '../components/CourseScoreboard.vue';
+  import Container from '../components/Container.vue';
 
   export default defineComponent({
     components: {
       CourseHeader,
       CourseScoreboard,
+      Container,
     },
     provide: {},
     setup() {},
@@ -22,7 +24,6 @@
 <style scoped>
   .container {
     flex: 1;
-    max-width: 90%;
     align-self: flex-start;
     margin: 10px -10px;
     overscroll-behavior: none;
